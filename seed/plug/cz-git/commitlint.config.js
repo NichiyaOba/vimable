c8b06ee1170dbb4f784ce9a6e89deef88cf00bb0
NichiyaOba <163647167+NichiyaOba@users.noreply.github.com>
2026-02-17 09:43:10 +0900

    feat: :sparkles: CIのロゴ変更

--- seed/plug/cz-git/commitlint.config.js
+++ seed/plug/cz-git/commitlint.config.js
@@ -1,30 +1,53 @@
 // @ts-check
-const { defineConfig } = require('cz-git')
-
-module.exports = defineConfig({
+module.exports = {
   rules: {
-    'type-enum': [2, 'always', [
-      'feat', 'fix', 'docs', 'style', 'refactor',
-      'perf', 'test', 'build', 'ci', 'chore', 'revert',
-    ]],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
   },
   prompt: {
     types: [
-      { value: 'feat',     name: 'feat:     ✨ 新機能', emoji: ':sparkles:' },
-      { value: 'fix',      name: 'fix:      🐛 バグ修正', emoji: ':bug:' },
-      { value: 'docs',     name: 'docs:     📝 ドキュメント', emoji: ':memo:' },
-      { value: 'style',    name: 'style:    💄 スタイル', emoji: ':lipstick:' },
-      { value: 'refactor', name: 'refactor: ♻️  リファクタリング', emoji: ':recycle:' },
-      { value: 'perf',     name: 'perf:     ⚡ パフォーマンス改善', emoji: ':zap:' },
-      { value: 'test',     name: 'test:     ✅ テスト', emoji: ':white_check_mark:' },
-      { value: 'build',    name: 'build:    📦 ビルド', emoji: ':package:' },
-      { value: 'ci',       name: 'ci:       👷 CI', emoji: ':construction_worker:' },
-      { value: 'chore',    name: 'chore:    🔧 雑務', emoji: ':wrench:' },
-      { value: 'revert',   name: 'revert:   ⏪ リバート', emoji: ':rewind:' },
+      { value: 'feat', name: 'feat:    ✨ 新機能', emoji: ':sparkles:' },
+      { value: 'fix', name: 'fix:      🐛 バグ修正', emoji: ':bug:' },
+      { value: 'docs', name: 'docs:    📝 ドキュメント', emoji: ':memo:' },
+      { value: 'style', name: 'style:  💄 スタイル', emoji: ':lipstick:' },
+      {
+        value: 'refactor',
+        name: 'refactor: ♻️  リファクタリング',
+        emoji: ':recycle:',
+      },
+      {
+        value: 'perf',
+        name: 'perf:     ⚡ パフォーマンス改善',
+        emoji: ':zap:',
+      },
+      {
+        value: 'test',
+        name: 'test:     ✅ テスト',
+        emoji: ':white_check_mark:',
+      },
+      { value: 'build', name: 'build:    📦 ビルド', emoji: ':package:' },
+      { value: 'ci', name: 'ci:       🎡 CI', emoji: ':ferris_wheel:' },
+      { value: 'chore', name: 'chore:    🔧 その他', emoji: ':wrench:' },
+      { value: 'revert', name: 'revert:   ⏪ リバート', emoji: ':rewind:' },
     ],
     useEmoji: true,
     allowCustomScopes: true,
     allowEmptyScopes: true,
     allowBreakingChanges: ['feat', 'fix'],
   },
-})
+};
